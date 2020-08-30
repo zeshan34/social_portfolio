5ba37d19fdfed8d0da610de829f23a4c7131b5f5
shaikhzeshan@gmail.com <shaikhzeshan@gmail.com>
2020-08-29 20:55:48 -0500

    setup express , morgan and stage the server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require ('express');
+// Morgan middleware library to log server requests, and you used Express's built-in express.static middleware to serve static HTML files.
+const morgan =require('morgan');
+const hostname ='localhost';
+const port = 3000;
+
+const app = express();
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname+'public'));
+
+
+app.use((req,res) =>{
+    console.log(req.header);
+    res.statusCode=200;
+    res.setHeader('Content-type', 'text/html');
+    res.end(`<html><body><h1>this is an express server</h1></body></html>`);
+});
+
+app.listen(port, hostname, () =>{
+    console.log(`server running at http://${hostname}:${port}`);
+});
